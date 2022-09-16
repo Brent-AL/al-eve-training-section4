ea6c5e243cdcab93d0269eca1d9cbf1322ab9a1c
Brent-AL <100272583+Brent-AL@users.noreply.github.com>
2022-09-16 08:33:23 +0200

    Added sign up

--- components/AuthForm/index.tsx
+++ components/AuthForm/index.tsx
@@ -1,49 +1,95 @@
-import styles from "@/styles/Forms.module.css"
-import { FormEvent,  useState } from "react"
-import {signIn} from "next-auth/react"
-
+import styles from "@/styles/Forms.module.css";
+import { FormEvent, useState } from "react";
+import { signIn } from "next-auth/react";
 
 function AuthForm() {
-    const [isSignin, setIssignin] = useState<boolean>(false)
+  const [isSignin, setIssignin] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("brent");
+  const [password, setPassword] = useState<string>("123");
 
+  const emailChangeHandler = (event: string) => {
+    setEmail(event.target.value);
+    console.log(event.target.value);
+  };
 
-    function switchAuthHandler(){
-        setIssignin((prevState)=>!prevState)
+  const passwordChangeHandler = (event: string) => {
+    setPassword(event.target.value);
+    console.log(event.target.value);
+  };
+
+  function switchAuthHandler() {
+    setIssignin((prevState) => !prevState);
+  }
+
+  async function createUser(email, password) {
+    const response = await fetch("api/auth/signup", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: {
+        "Content-Type": "application-json",
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message || "Something went wrong");
     }
 
+    return data;
+  }
 
-    
-    async function submitHandler(event:FormEvent<HTMLFormElement>){
-        event.preventDefault()
+  async function submitHandler(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
 
-        if(isSignin){
-            // Question 7.1.3
-        }else{
-            // Question 7.1.2
-        }
-
+    if (isSignin) {
+      // Question 7.1.3
+      //   log user in
+    } else {
+      // Question 7.1.2
+      //   create user
+      try {
+        const result = await createUser(email, password);
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
     }
-    
-    
+  }
+
   return (
     <form onSubmit={submitHandler} className={styles.form}>
-        <h2>{isSignin ? 'Sign in':'Sign up'}</h2>
-        <div className={styles["control-group"]}>
-            <div className={styles["form-control"]}>
-                <label htmlFor="username">User Name</label>
-                <input  type="text" id="username" required />
-            </div>
-            <div className={styles["form-control"]}>
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" required />
-            </div>
+      <h2>{isSignin ? "Sign in" : "Sign up"}</h2>
+      <div className={styles["control-group"]}>
+        <div className={styles["form-control"]}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            id="email"
+            required
+            value={email}
+            onChange={emailChangeHandler}
+          />
         </div>
-        <div className={styles["form-actions"]}>
-            <button type="submit">Submit</button>
-            <a onClick={switchAuthHandler}>{!isSignin ? 'Sign in':'Sign up'} instead?</a>
+        <div className={styles["form-control"]}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            value={password}
+            onChange={passwordChangeHandler}
+          />
         </div>
+      </div>
+      <div className={styles["form-actions"]}>
+        <button type="submit">Submit</button>
+        <a onClick={switchAuthHandler}>
+          {!isSignin ? "Sign in" : "Sign up"} instead?
+        </a>
+      </div>
     </form>
-  )
+  );
 }
 
-export default AuthForm
+export default AuthForm;
--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,25 +1,54 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { EMAIL, PASSWORD } from "constant/validations";
 import { connectToUsers } from "utils/dbEmulator";
-import {hashPassword} from "utils/encryption"
+import { hashPassword } from "utils/encryption";
 
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    return;
+  }
 
-async function handler(req:NextApiRequest, res:NextApiResponse) 
-{   
+  const data = req.body;
 
-    if (req.method !== 'POST'){
-        return 
-    }
+  let { email, password } = data;
 
-    // Question 7.2
+  //     if (!email || !password) {
+  //       res.status(422).json({
+  //         message: "Invalid input - password should be x characters long.",
+  //       });
+  //       return;
+  //     }
 
+  email = "b@b.com";
+  password = "123";
+
+  const client = await connectToUsers();
+
+  const db = client?.db();
+
+  const exisitingUser = await db?.findOne(email);
+
+  if (exisitingUser) {
+    res.status(422).json({
+      message: "User already exists",
+    });
+    db?.close;
+    return;
+  }
+
+  const hashedPassword = await hashPassword(password);
+
+  db?.insertOne({
+    email: email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({ message: "Created user! " });
+  db?.close;
+  // Question 7.2
 }
 
-export default handler
-
-
-
-
+export default handler;
 
 /**
  * @EMAIL & @PASSWORD
@@ -28,29 +57,22 @@ export default handler
  * @returns  {boolean}
  */
 
-
-
 /**
  * @connectToUsers
  * It returns an object with a function that returns a new instance of the dbEmulator class.
  * @returns An object with a function called @db that returns a new instance of the @dbEmulator class.
  */
 
-
-
-/** 
-* @dbEmulator class
-* It's a class that emulates a database, it has a private array of users, and it has methods to add
-* users, get all users, and find a user by email. 
-* Methods:
-* @insertOne : a @function that takes an object of { type=string ,password type=string} and adds the user to the database.
-* @findOne : a @function that takes a string and @returns an object of {email type=string ,password type=string} and @returns null if no user is found
-* @open : a @function that emulates opening a connection to the db. This is required to read and write data.
-* @close : a @function that emulates closing a connection to the db is required for syncing data.
-*/
-
-
-
+/**
+ * @dbEmulator class
+ * It's a class that emulates a database, it has a private array of users, and it has methods to add
+ * users, get all users, and find a user by email.
+ * Methods:
+ * @insertOne : a @function that takes an object of { type=string ,password type=string} and adds the user to the database.
+ * @findOne : a @function that takes a string and @returns an object of {email type=string ,password type=string} and @returns null if no user is found
+ * @open : a @function that emulates opening a connection to the db. This is required to read and write data.
+ * @close : a @function that emulates closing a connection to the db is required for syncing data.
+ */
 
 /**
  * @hashPassword
